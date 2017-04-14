81228914559adcfb79d334ebae1a9b2e64ee5fce
Michael L Parks <github@wmi-inc.com>
2017-04-14 15:44:39 -0400

    Initial Commit

---
+++ footer2.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Footer</title>
+    <link rel="stylesheet" href="./fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="./css/footer-responsive.css">
+  </head>
+  <body>
+    <footer>
+      <div class="footer-bottom">
+        <div class="footer-bottom-copyright-text">
+          &copy; 2017 Michael L Parks | All Rights Reserved
+        </div>
+        <div class="footer-bottom-social-icons">
+            <ul id="footer-social-links">
+              <li><a href="https://www.youtube.com/channel/UC3dDgmi3FuwLz02EU3dZVag" target="_blank"><i class="fa fa-youtube-square fa-2x" aria-hidden="true"></i></a></li>
+              <li><a href="https://www.facebook.com/whollycoder/" target="_blank"><i class="fa fa-facebook-square fa-2x" aria-hidden="true"></i></a></li>
+              <li><a href="https://twitter.com/michaellparks" target="_blank"><i class="fa fa-twitter-square fa-2x" aria-hidden="true"></i></a></li>
+              <li><a href="https://www.linkedin.com/in/michaellawrenceparks/" target="_blank"><i class="fa fa-linkedin-square fa-2x" aria-hidden="true"></i></a></li>
+              <li><a href="https://github.com/WhollyCoders" target="_blank"><i class="fa fa-github-square fa-2x" aria-hidden="true"></i></a></li>
+            </ul>
+        </div>
+      </div>
+    </footer>
+  </body>
+</html>
+<?php
+// <li>
+//   <a href="https://www.facebook.com/michael.l.parks" target="_blank">
+//     <i class="fa fa-facebook-square fa-2x" aria-hidden="true"></i>
+//   </a>
+// </li>
+?>
